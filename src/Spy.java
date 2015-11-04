3ed8a1c9be50ddd036844fd9f24324435570cb0c
Brandon Nguyen <happytreefriends001@yahoo.com>
2015-11-03 21:02:47 -0800

    fix

--- src/Spy.java
+++ src/Spy.java
@@ -1,17 +1,25 @@
-/**
- * 
- */
-
-/**
- * @author Brandon Nguyen
- *
- */
 public class Spy extends BoardPiece {
+	
+	private int ammoCount;
+	
+	private int numberOfLives;
 
 	public Spy(String s) {
 		super(s);
 		super.setIsVisible(true);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void setAmmoCount(){
+		
+	}
+	
+	public void setNumberOfLives(){
+		
+	}
+	
+	public int getNumberOfLives(){
+		return numberOfLives;
 	}
 
 }
