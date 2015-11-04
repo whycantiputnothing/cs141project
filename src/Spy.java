7fc9e052254ace0e26883ed5c6e521be63351f9a
whycantiputnothing <happytreefriends001@yahoo.com>
2015-11-03 20:51:26 -0800

    update

--- src/Spy.java
+++ src/Spy.java
@@ -7,11 +7,18 @@
  *
  */
 public class Spy extends BoardPiece {
+	
+	private int ammoCount;
+	
 
 	public Spy(String s) {
 		super(s);
 		super.setIsVisible(true);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void setAmmoCount(){
+		
 	}
 
 }
