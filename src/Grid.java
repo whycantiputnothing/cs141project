f2faa88683d5d85fe83e0a9fa0c1d9d67251ff65
Brandon Nguyen <happytreefriends001@yahoo.com>
2015-11-03 19:20:05 -0800

    First code

--- src/Grid.java
+++ src/Grid.java
@@ -1,11 +1,19 @@
-/**
- * 
- */
 
-/**
- * @author Brandon Nguyen
- *
- */
 public class Grid {
+
+    public Grid() {
+	}
+	
+	public void startGrid(){
+		
+	}
+	
+	public String printGrid(){
+		return null;
+	}
+	
+	public void getMovement(){
+		
+	}
 
 }
--- src/UI.java
+++ src/UI.java
@@ -1,11 +1,25 @@
-/**
- * 
- */
 
-/**
- * @author Brandon Nguyen
- *
- */
 public class UI {
+
+    public UI() {
+	}
+	
+	public void startGame(){
+		
+	}
+	
+	public void optionMenu(){
+		
+	}
+	
+	public void deadMessage(){
+		
+	}
+	
+	public static void gameLoop(){
+		
+	}
+	
+	
 
 }
