1324b6ff1dec79de967329c7d2f1b835178ec88b
Brandon Nguyen <happytreefriends001@yahoo.com>
2015-11-09 11:31:31 -0800

    working grid instantiation

--- src/Grid.java
+++ src/Grid.java
@@ -6,7 +6,28 @@ public class Grid {
 	 
 		
 	public void instantiateGrid(){
-		
+		int x = 0;
+		for (int i = 0; i < grid.length; i++){
+			for (int j = 0; j < grid[i].length; j++){
+				if (x < 68){
+					grid[i][j] = new BoardPiece("E");
+				}
+			}
+		}
+		grid[7][5] = new Ninja();
+		grid[7][6] = new Ninja();
+		grid[7][7] = new Ninja();
+		grid[7][8] = new Spy();
+		grid[8][0] = new Room(false);
+		grid[8][1] = new Room(false);
+		grid[8][2] = new Room(false);
+		grid[8][3] = new Room(false);
+		grid[8][4] = new Room(false);
+		grid[8][5] = new Room(false);
+		grid[8][6] = new PowerUps("B");
+		grid[8][7] = new PowerUps("I");
+		grid[8][8] = new PowerUps("R");
+	
 		
 	}
 	
@@ -21,7 +42,13 @@ public class Grid {
 	
 	public String toString() {
 		String printPosition = "";
-		 
+		 for (BoardPiece[] b : grid){
+			 for (BoardPiece p : b){
+				 printPosition += "[" + p.getPieceType() + "]";
+			 }
+			 printPosition += "\n";
+			 
+		 }
 		return printPosition;
 		
 	}
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args){
     	Grid g = new Grid();
     	g.instantiateGrid();
-    	System.out.println("hello world");
     	String str = g.toString();
     	System.out.println(str);
 		
